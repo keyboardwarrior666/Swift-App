dcd16f83deeaca336a6c85652860ec4801b7da58
Johan Scholtz <johan_scholtz@outlook.com>
2017-03-02 15:31:31 +0200

    Test

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -6,15 +6,37 @@
 //  Copyright © 2017 Johan Scholtz. All rights reserved.
 //
 
+
+//New comment
+
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    var tapCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        theLabel.text = "Hello nooob!"
+        tapCount = tapCount + 1
+        
+        
+        if tapCount >= 10{
+            theLabel.text = "tittes"
+        }
     }
 
+    @IBAction func buttonTappedTwo(_ sender: Any) {
+        theLabel.textColor = UIColor.blue
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
